ee1980db9ce352d5fdc662ebde62a1ba9c6a9b6a
RamNikhil411 <nikhil.b@orotron.com>
2025-10-06 13:55:13 +0530

    Add nitro

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,19 @@
 import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import tailwindcss from "@tailwindcss/vite";
 import viteReact from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { nitroV2Plugin } from "@tanstack/nitro-v2-vite-plugin";
 
 export default defineConfig({
   server: {
     port: 3000,
   },
   plugins: [
-    tsConfigPaths(),
-    tanstackStart({ customViteReactPlugin: true }),
-    viteReact(),
     tailwindcss(),
+    tsConfigPaths(),
+    tanstackStart(),
+    viteReact(),
+    nitroV2Plugin(),
   ],
 });
