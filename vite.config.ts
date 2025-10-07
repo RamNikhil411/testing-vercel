3d8f8530e4fc54255d8e59a78fbda326d3ad19a1
RamNikhil411 <nikhil.b@orotron.com>
2025-10-07 11:18:43 +0530

    update vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tsConfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import netlify from "@netlify/vite-plugin-tanstack-start";
+import { nitroV2Plugin } from "@tanstack/nitro-v2-vite-plugin";
 
 export default defineConfig({
   server: {
@@ -14,7 +14,9 @@ export default defineConfig({
     tailwindcss(),
     tsConfigPaths(),
     tanstackStart(),
-    netlify(),
+    nitroV2Plugin({
+      preset: "vercel",
+    }),
     viteReact(),
   ],
 });
