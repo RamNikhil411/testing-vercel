910b5b7e834a979e6477b38e568df51fa87094b0
RamNikhil411 <nikhil.b@orotron.com>
2025-10-07 11:07:25 +0530

    Update netlify

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,17 @@ import tsConfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import netlify from "@netlify/vite-plugin-tanstack-start";
 
 export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [tailwindcss(), tsConfigPaths(), tanstackStart(), viteReact()],
+  plugins: [
+    tailwindcss(),
+    tsConfigPaths(),
+    tanstackStart(),
+    netlify(),
+    viteReact(),
+  ],
 });
